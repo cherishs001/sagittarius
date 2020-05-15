343346c2c62b3d8e43ea40a44ced7eaa52e4c939
kaiyin <kaiyin@gaotengasset.com>
2020-05-15 17:57:18 +0800

    feat(文件上传): 解析文件上传时的路径问题

--- test/services/login.ts
+++ test/services/login.ts
@@ -1,24 +1,33 @@
 import {Context, Orm, Service, Schema} from '../../lib';
 
+const struct = Schema.superstruct({
+    types: {
+        any: (value: any) => {
+            return true;
+        }
+    },
+});
+
 export default class Login extends Service {
     path: string = '/api/v1/login';
-    get_schema: Schema.Struct = Schema.struct({
-        id: 'number',
+    post_schema: Schema.Struct = struct({
+        file: 'any',
     });
 
     async GET(ctx: Context): Promise<void> {
-        const {id} = ctx.params;
         ctx.logs.debug(`debug info`);
+
         ctx.info = '123';
         ctx.body = {
-            id: id,
+            id: 123,
         };
     };
 
     async POST(ctx: Context): Promise<void> {
-        const {id} = ctx.params;
+        const {file} = ctx.params;
+        console.log(file);
         ctx.body = {
-            id: id,
+            id: 123,
         };
     }
 }
